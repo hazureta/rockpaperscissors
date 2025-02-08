a543ff99a2ea3c33e8aa233da5979f6c21722895
hazureta <77740821+hazureta@users.noreply.github.com>
2025-02-08 16:01:02 +0300

    Reworked to play the game using buttons
    Was unbelievably lazy, and added all of the replay logic without explicitly adding it - just put everything in the event listener, so that every time you press a button - the computer choice is run all over again.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let computerChoice = undefined
-let userChoice = undefined
+let computerChoice = ''
+let userChoice = ''
 
 function randomOfThree() {
     let choice = Math.floor(Math.random() * 3) + 1
@@ -12,29 +12,27 @@ function randomOfThree() {
     }
 }
 
-randomOfThree()
+// function userInput() {
+//     choice = prompt(`Gimme rock, paper, or scissors. And remember - no reefunds - so don't make typos ya dingo`, ``)
+//     if (choice === `rock`) {
+//         userChoice = `rock`
+//     } else if (choice === `paper`) {
+//         userChoice = `paper`
+//     } else if (choice === `scissors`) {
+//         userChoice = `scissors`
+//     } else if (choice === null) {
+//         alert(`What the fack are ya doin ya dingo... you cancelled da game!`)
+//         userInput()
+//     } else if (choice === "") {
+//         alert(`Now how is that OK YA DINGO???!!`)
+//         userInput()
+//     } else {
+//         alert(`Ya fackin made a typo ya dingo`)
+//         userInput()
+//     }
+// }
 
-function userInput() {
-    choice = prompt(`Gimme rock, paper, or scissors. And remember - no reefunds - so don't make typos ya dingo`, ``)
-    if (choice === `rock`) {
-        userChoice = `rock`
-    } else if (choice === `paper`) {
-        userChoice = `paper`
-    } else if (choice === `scissors`) {
-        userChoice = `scissors`
-    } else if (choice === null) {
-        alert(`What the fack are ya doin ya dingo... you cancelled da game!`)
-        userInput()
-    } else if (choice === "") {
-        alert(`Now how is that OK YA DINGO???!!`)
-        userInput()
-    } else {
-        alert(`Ya fackin made a typo ya dingo`)
-        userInput()
-    }
-}
-
-userInput()
+// userInput()
 
 function choiceComparison() {
     if (computerChoice === userChoice) {
@@ -54,23 +52,53 @@ function choiceComparison() {
     }
 }
 
-choiceComparison()
 
-function replayPrompt() {
-    let choice = prompt(`Does yee want to engage in a replay battle of the minds most wanderous - rock paper scissors? \n Input anything except "yo momma", and you'll replay.`)
-    if (choice === `yo momma`) {
-        alert(`My momma? How about DEEZ NUTS?`)
-        replayFunctionality()
-    } else {
-        replayFunctionality()
-    }
-}
 
-function replayFunctionality() {
+// function replayPrompt() {
+//     let choice = prompt(`Does yee want to engage in a replay battle of the minds most wanderous - rock paper scissors? \n Input anything except "yo momma", and you'll replay.`)
+//     if (choice === `yo momma`) {
+//         alert(`My momma? How about DEEZ NUTS?`)
+//         replayFunctionality()
+//     } else {
+//         replayFunctionality()
+//     }
+// }
+
+// function replayFunctionality() {
+//     randomOfThree()
+//     userInput()
+//     choiceComparison()
+//     replayPrompt()
+// }
+
+// replayPrompt()
+
+const div = document.querySelector('div')
+
+const btnRock = document.createElement("button")
+const btnPaper = document.createElement("button")
+const btnScissors = document.createElement("button")
+
+btnRock.textContent = 'Rock'
+btnPaper.textContent = "Paper"
+btnScissors.textContent = "Scissors"
+
+div.appendChild(btnRock)
+div.appendChild(btnPaper)
+div.appendChild(btnScissors)
+
+btnRock.addEventListener('click', () => {
+    userChoice = 'rock'
     randomOfThree()
-    userInput()
     choiceComparison()
-    replayPrompt()
-}
-
-replayPrompt()
+})
+btnPaper.addEventListener('click', () => {
+    userChoice = 'paper';
+    randomOfThree()
+    choiceComparison()
+});
+btnScissors.addEventListener('click', () => {
+    userChoice = 'scissors';
+    randomOfThree()
+    choiceComparison()
+});
